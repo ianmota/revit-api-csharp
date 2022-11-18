c25e8d4c2004152092120b703a501c55f67c2808
Ian Mota <ianmotabr@gmail.com>
2022-11-18 17:44:46 -0300

    add-selector-group

--- GroupOrganization.cs
+++ GroupOrganization.cs
@@ -11,6 +11,7 @@ using Autodesk.Revit.UI.Selection;
 
 namespace revit_api_csharp
 {
+    /*
     public class PickGroup : ISelectionFilter
     {
         public bool AllowElement(Element elem)
@@ -23,22 +24,43 @@ namespace revit_api_csharp
             return false;
         }
     }
+    */
 
     [Transaction(TransactionMode.Manual)]
     internal class GroupOrganization : IExternalCommand
-    {
+    { 
         public Result Execute(ExternalCommandData commandData, ref string message, ElementSet elements)
         {
             // Get application and document objets
             UIDocument uidoc = commandData.Application.ActiveUIDocument;
             Document doc = uidoc.Document;
 
+            /*
             Reference pickedObj = null;
 
             //Pick a group
             Selection sel = uidoc.Selection;
             PickGroup selGroup = new PickGroup();
             pickedObj = sel.PickObject(ObjectType.Element, selGroup, "Please, select a group");
+            */
+
+            IList<Group> grps = new FilteredElementCollector(doc)
+                .OfClass(typeof(Group))
+                .Cast<Group>()
+                .ToList();
+
+
+            IList<XYZ> grpPoint = new List<XYZ>();
+
+            foreach (Group grp in grps)
+            {
+                LocationPoint grpLocation = grp.Location as LocationPoint;
+
+                grpPoint.Add(grpLocation.Point);
+
+            }
+
+                TaskDialog.Show("Grupos no Modelo", string.Format("TOTAL DE GRUPOS: {0}",grps.Count.ToString()));
 
 
             return Result.Succeeded;
